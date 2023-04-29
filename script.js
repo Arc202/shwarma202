410f0be651ba192dabbb2fbaf95212890574629e
Arc202 <130196372+Arc202@users.noreply.github.com>
2023-04-29 11:18:29 -0700

    so uhm
    this is some stuff 
    (better not be watching cory)

---
+++ script.js
@@ -0,0 +1,11 @@
+function insert() {
+var password = "shopkeeper2";
+var input = document.getElementById('password').value;
+if (input==password)  {
+alert ('Correct password')
+
+}
+else {
+alert ('wrong password please try again')
+}
+}
